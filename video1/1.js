410ac52b886216bb9ac92fa57e754fd4e251555f
kashif0426 <82613468+kashif0426@users.noreply.github.com>
2023-04-05 11:59:08 +0200

    js tutorial

---
+++ video1/1.js
@@ -0,0 +1,15 @@
+// ---------------------------- alert(); -------------------------------------------------
+// alert("Welcome");
+// alert(25);
+
+// ---------------------------- document.write(); -------------------------------------------------
+// document.write("Hello world");
+// document.write("<h1>Hello world</h1>");
+
+// ---------------------------- console.log(); -------------------------------------------------
+// console.log("hello world");
+
+//homework
+
+document.write("First: 'Hello world' <br>");
+document.write("Seconde: 'Goodbye world'");
